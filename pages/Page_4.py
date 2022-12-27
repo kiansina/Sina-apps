27ebf02d8d5d9ef03d0615ba36a3db663d766561
Sina Kian <70090343+kiansina@users.noreply.github.com>
2022-12-27 15:36:17 +0100

    Create Page_4.py

---
+++ pages/Page_4.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+
+
+
+
+
+st.markdown("# Dashboard 📈")
+st.sidebar.markdown("# Dashboard 📈")
+
+st.markdown('In addition to Tableau, and streamlit, I can develope dashboards using Dash and plotly libraries of python')
+st.warning("After clicking on the link below, wait a bit until it loads completely")
+st.markdown('You can find an example here http://sena.pythonanywhere.com/')
+
