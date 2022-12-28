e5e384d88a48d69c4510187d8d4cd625a0cd94a8
Sina Kian <70090343+kiansina@users.noreply.github.com>
2022-12-28 09:13:23 +0100

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -12,7 +12,8 @@ st.set_page_config(page_title="Sina-Apps", page_icon=img)
 st.markdown("# Who I am 🎈")
 st.sidebar.markdown("# Welcome 🎈")
 
-st.markdown(':blue[For every need please contact with **_sina.kian@mail.polimi.it_** \n I will develope applications to ease the work]🚧🚧🚧')
+st.markdown(':blue[For every need please contact with **_sina.kian@mail.polimi.it_**]')
+st.markdown(':blue[I will develope applications to ease the work]🚧🚧🚧')
 st.markdown('Here I attach some demo apps as examples. I develope specific applications for daily, boring or complex duties.')
 st.markdown(f"###### :blue[Your problems are like a piece of cake!] 🍰 \n"
             f"###### :blue[You announce your problem, I eat it.] 😉") 
